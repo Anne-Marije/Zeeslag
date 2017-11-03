609bdb131da6fcc8a889e2803137832ff03afb34
Anne-Marije <napannemarije@gmail.com>
2017-11-03 12:26:19 +0100

    veldmetknoppen

---
+++ index.php
@@ -0,0 +1,132 @@
+<html>
+    <head>
+        <script>
+        function kleur (){
+            alert ("test");
+            document.getElementsByClassName("button").innerHTML = "RAAK";
+            document.getElementsByClassName("button").style.backgroundColor = "red";
+        }
+
+        </script>
+        <style>
+           
+            .button:hover{background-color: tomato}
+           
+           .button {
+    background-color: darkslategray;
+    border: none;
+    color: white;
+    padding: 10px 10px;
+    cursor: crosshair;
+     
+}
+
+            
+        </style>
+        
+        
+    </head>
+    
+    <body>
+        <p>Zeeslag</p>
+     
+     
+    <table>
+            <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+ <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button"class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+             <tr>
+                <?php
+                for($x =0;$x<10;$x++){
+                    echo '<td><button onclick="kleur()" "type="button" class="button">_</button></td>';
+
+                }
+                
+                ?>
+            </tr>
+            
+    </table>
+        
+    </body>
+    
+    
+    
+</html>
